9e74b909ac058ad7be88eeb573fd14d4fd1a989e
Giovanny Gonzalez <ing.hggw@gmail.com>
2022-04-04 11:00:00 -0500

    fix: minusculas y mayusculas

--- View/login/index.php
+++ View/login/index.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../public/css/style_index.css">
-  <link rel="stylesheet" href="../public/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../Public/css/style_Index.css">
+  <link rel="stylesheet" href="../Public/css/bootstrap.min.css">
   <link rel="shortcut icon" href="../Public/image/burger_fast_food_icon_181517.png">
   <title>Food soft</title>
 </head>
